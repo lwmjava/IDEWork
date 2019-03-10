e937d0f632bce6218682b0ed4aa78c8e5ad2f216
liuwenming <liuwenming@sfhc.onaliyun.com>
2019-03-10 23:02:17 +0800

    Initial commit

---
+++ Module01/src/com/lwm/bean/Custom.java
@@ -0,0 +1,17 @@
+package com.lwm.bean;
+
+import com.sun.deploy.util.ArrayUtil;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Custom {
+
+    private static final int initialsize = 10;
+    public static void main(String[] args){
+        System.out.println("HelloWorld");
+        ArrayList list = null;
+        Date date = new Date();
+    }
+}
